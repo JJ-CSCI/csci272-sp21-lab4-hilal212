3d33d38d28bc1ea1d1d78b63a4e14f7076fe5340
hilalavci21 <PSK.HILAL21@HOTMAIL.COM>
2021-03-09 20:02:10 +0000

    I wrote a class that defines a complex number and used streaming operations for formatting string output and parsing.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,78 @@
 //------------------------------
-//  DO NOT MODIFY NEXT LINE
+// DO NOT MODIFY NEXT LINE
 //------------------------------
 #include <string>
 #include "catch.hpp"
 using Catch::Matchers::Equals;
-//------------------------------
+//-----------------------------
 
-// Fix the following class
-class Complex {
-    void operator>>(std::string&) const;
-    void operator<<(const std::string&);
+  class Complex {
+ {
+private:
+ int real, imaginary;
+Complex(int real=1.0,int imaginary=0);
+int real();
+int imaginary();
+
+void operator >> (std::string&) const;
+void operator << (const std::string&);
+
 };
 
-//------------------------------
+//Complex.cpp
+#include "Complex.h"
+
+
+Complex::Complex(int real, int imaginary)
+{
+this->real = real;
+this->imaginary= imaginary;
+}
+
+int Complex::real()
+{
+return this->real;
+}
+
+int Complex::imaginary()
+{
+return this->imaginary;
+}
+
+void Complex::operator>>(std::string& s) const
+{
+s.append(std::to_string(this->real));
+if (this->imaginary > 0)
+{
+s.append("+");
+ }
+s.append(std::to_string(this->imaginary));
+s.append("i");
+}
+
+void Complex::operator<<(const std::string& s)
+{
+if (s[0] == '-') {
+int real= s[1] - '0';
+this->real = -1 * real;
+}
+else
+{
+this->real = s[0] - '0'; 
+}
+
+int ipos = s.find("i");
+int bpos = ipos - 1;
+int bsignpos = ipos - 2;
+if (s[bsignpos] == '-')
+{
+this->imaginary = -1 * (s[bpos'0');
+}
+ else
+ {
+ this->imaginary =s[bpos] -'0';
+ }
+};
 //   DO NOT MODIFY TEST CASES
 //------------------------------
 TEST_CASE( "Assignment" ) {
